0e65ce706762183f495fb591ed8140a523575d03
Ivaylo Vasilev <ivaylovasilev81@gmail.com>
2025-03-26 03:18:05 +0200

    Initial commit of chksum and mksum

---
+++ chksum.py
@@ -0,0 +1,159 @@
+import hashlib
+import argparse
+import sys
+import os
+from colorama import init, Fore
+
+init(autoreset=True)
+OK = str(Fore.GREEN + "[+]" + Fore.RESET)
+NO = str(Fore.RED + "[-]" + Fore.RESET)
+ERR = str(Fore.RED + "Error:" + Fore.RESET)
+
+BANNER = """
+
+            ##########################
+                   chksum 1.0.5
+                ------------------
+                        *
+              (c)2025 Ivaylo Vasilev
+            ##########################
+            
+        """
+
+# Parse the arguments from command line
+parser = argparse.ArgumentParser(prog="chksum", description="chksum - verify file integrity", epilog="(c) Ivaylo Vasilev")
+parser.add_argument("file", nargs="?", metavar="FILE", help="specify file")
+parser.add_argument("-v", "--value", metavar="HASH_VALUE", help="enter hashed value")
+parser.add_argument("--version", action="version", version="%(prog)s 1.0.5", help="show program version")
+args = parser.parse_args()
+
+
+def main():
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    
+    # Perform error checking
+    if not args.file:
+        sys.exit(f"{ERR} Missing required argument: FILE")
+    if not os.path.isfile(args.file):
+        sys.exit(f"{ERR} {args.file} is not a file")
+    if not args.value:
+        sys.exit(f"{ERR} Missing required argument: HASH_VALUE")
+    
+    print(BANNER)
+    
+    # Getting name and size information for the file
+    file_name = os.path.basename(args.file)
+    file_size = os.path.getsize(args.file)
+
+    print("File information:")
+    print("=================")
+    print(f"Name: {file_name}")
+    print(f"Size: {float(file_size / 1024 / 1024):.2f} MB")
+    print("")
+    print("Checking file ...")
+    print("")
+
+    # Open the file and read as binary to create a proper format for hash value library; needs data as bytes
+    with open(args.file, "rb") as f:
+        file = f.read()
+    hashvalue = args.value
+
+    # Specify the hash algorithm
+    if len(args.value) == 32:
+        print("Hash: MD5")
+        print("=========")
+        check = md5sum(file, hashvalue)
+        if check == True:
+            print(f"{OK} File integrity confirmed")
+        else:
+            print(f"{NO} File integrity not confirmed")
+    elif len(args.value) == 40:
+        print("Hash: SHA-1")
+        print("===========")
+        check = sha1sum(file, hashvalue)
+        if check == True:
+            print(f"{OK} File integrity confirmed")
+        else:
+            print(f"{NO} File integrity not confirmed")
+    elif len(args.value) == 56:
+        print("Hash: SHA-224")
+        print("=============")
+        check = sha224sum(file, hashvalue)
+        if check == True:
+            print(f"{OK} File integrity confirmed")
+        else:
+            print(f"{NO} File integrity not confirmed")
+    elif len(args.value) == 64:
+        print("Hash: SHA-256")
+        print("=============")
+        check = sha256sum(file, hashvalue)
+        if check == True:
+            print(f"{OK} File integrity confirmed")
+        else:
+            print(f"{NO} File integrity not confirmed")
+    elif len(args.value) == 128:
+        print("Hash: SHA-512")
+        print("=============")
+        check = sha512sum(file, hashvalue)
+        if check == True:
+            print(f"{OK} File integrity confirmed")
+        else:
+            print(f"{NO} File integrity not confirmed")
+    else:
+        sys.exit(f"{ERR} Unknown hash type for value: {args.value}")
+
+
+def md5sum(file, hashvalue):
+    hashobject = hashlib.md5()
+    hashobject.update(file)
+
+    if hashobject.hexdigest() == hashvalue:
+        return True
+    else:
+        return False
+
+
+def sha1sum(file, hashvalue):
+    hashobject = hashlib.sha1()
+    hashobject.update(file)
+
+    if hashobject.hexdigest() == hashvalue:
+        return True
+    else:
+        return False
+
+
+def sha224sum(file, hashvalue):
+    hashobject = hashlib.sha224()
+    hashobject.update(file)
+
+    if hashobject.hexdigest() == hashvalue:
+        return True
+    else:
+        return False
+
+
+def sha256sum(file, hashvalue):
+    hashobject = hashlib.sha256()
+    hashobject.update(file)
+
+    if hashobject.hexdigest() == hashvalue:
+        return True
+    else:
+        return False
+
+
+def sha512sum(file, hashvalue):
+    hashobject = hashlib.sha512()
+    hashobject.update(file)
+
+    if hashobject.hexdigest() == hashvalue:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
---
+++ mksum.py
@@ -0,0 +1,116 @@
+import hashlib
+import argparse
+import sys
+import os
+
+parser = argparse.ArgumentParser(prog="mksum", description="Generate checksum hash values", epilog="(c) Ivaylo Vasilev")
+parser.add_argument("file", nargs="?", help="specify file")
+parser.add_argument("--hash", metavar="hash", default="md5", help="specify hash type")
+parser.add_argument("--list", action="store_true", help="show supported hash types")
+parser.add_argument("--version", action="version", version="%(prog)s 1.0.1", help="show program version")
+args = parser.parse_args()
+
+
+def main():
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    elif args.list:
+        print("mksum: --hash md5 | sha1 | sha224 | sha256 | sha384 | sha512 | all")
+        sys.exit(0)
+
+    if not os.path.isfile(args.file):
+        sys.exit("error: %s is not a file" % args.file)
+
+    file_name = os.path.basename(args.file)
+    file_size = os.path.getsize(args.file)
+    file_size = float(file_size / 1024 / 1024)
+
+    print("Generating checksum hash value(s)")
+    print("=================================")
+    print("File name: %s" % file_name)
+    print("File size: %.2f MB" % file_size)
+    print("")
+
+    with open(args.file, "rb") as f:
+        file = f.read()
+    
+    if args.hash == "md5":
+        print(*md5_hash(file))
+        print("")
+    elif args.hash == "sha1":
+        print(*sha1_hash(file))
+        print("")
+    elif args.hash == "sha224":
+        print(*sha224_hash(file))
+        print("")
+    elif args.hash == "sha256":
+        print(*sha256_hash(file))
+        print("")
+    elif args.hash == "sha384":
+        print(*sha384_hash(file))
+        print("")
+    elif args.hash == "sha512":
+        print(*sha512_hash(file))
+        print("")
+    elif args.hash == "all":
+        print(*md5_hash(file))
+        print("")
+        print(*sha1_hash(file))
+        print("")
+        print(*sha224_hash(file))
+        print("")
+        print(*sha256_hash(file))
+        print("")
+        print(*sha384_hash(file))
+        print("")
+        print(*sha512_hash(file))
+        print("")
+    else:
+        sys.exit("error: unknown or unsupported hash type '%s'" % args.hash)
+
+
+def md5_hash(file):
+    hashobject0 = hashlib.md5()
+    hashobject0.update(file)
+
+    return "MD5    :", hashobject0.hexdigest()
+
+
+def sha1_hash(file):
+    hashobject1 = hashlib.sha1()
+    hashobject1.update(file)
+
+    return "SHA-1  :", hashobject1.hexdigest()
+
+
+def sha224_hash(file):
+    hashobject2 = hashlib.sha224()
+    hashobject2.update(file)
+
+    return "SHA-224:", hashobject2.hexdigest()
+
+
+def sha256_hash(file):
+    hashobject3 = hashlib.sha256()
+    hashobject3.update(file)
+
+    return "SHA-256:", hashobject3.hexdigest()
+
+
+def sha384_hash(file):
+    hashobject4 = hashlib.sha384()
+    hashobject4.update(file)
+
+    return "SHA-384:", hashobject4.hexdigest()
+
+
+def sha512_hash(file):
+    hashobject5 = hashlib.sha512()
+    hashobject5.update(file)
+
+    return "SHA-512:", hashobject5.hexdigest()
+
+
+if __name__ == "__main__":
+    main()
